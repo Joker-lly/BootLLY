8976e940bdb1d51cc1a80f9718f1b6004b0aeb8e
Joker-lly <15230108390@163.com>
2020-03-28 10:47:52 +0800

    项目初建

---
+++ src/main/java/com/lly/LlyApplication.java
@@ -0,0 +1,13 @@
+package com.lly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lly/LlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
