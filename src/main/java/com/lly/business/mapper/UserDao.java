de072f1bca55aed85f349ded83044524fb82c696
Joker-lly <52774953+Joker-lly@users.noreply.github.com>
2020-06-03 11:17:54 +0800

    Update UserDao.java
    更改注释

--- src/main/java/com/lly/business/mapper/UserDao.java
+++ src/main/java/com/lly/business/mapper/UserDao.java
@@ -2,11 +2,11 @@ package com.lly.business.mapper;
 
 import com.lly.business.entity.UserEntity;
 import org.springframework.stereotype.Component;
-
+import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 import java.util.Map;
 
-@Component
+@Mapper
 public interface UserDao {
    // public UserEntity getUserinfor(String username);
 
