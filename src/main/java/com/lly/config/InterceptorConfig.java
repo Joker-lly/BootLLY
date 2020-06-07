4fd61d99692c048ddb5d78c7cc87c87ad8eb49a1
Joker-lly <15230108390@163.com>
2020-06-07 10:25:45 +0800

    拦截器代码完善

--- src/main/java/com/lly/config/InterceptorConfig.java
+++ src/main/java/com/lly/config/InterceptorConfig.java
@@ -3,113 +3,30 @@ package com.lly.config;
 import com.lly.interceptor.CheckLoginInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.Validator;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.*;
 
-import java.util.List;
-
+/**
+ * 登录拦截器
+ *
+ * @author Joker-lly
+ * @since 2020-06-07
+ */
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
-    //
+    // 注册自己的拦截器
     @Bean
     CheckLoginInterceptor checkLoginInterceptor(){
         return new CheckLoginInterceptor();
     }
 
-
-   /* @Override
+    /**
+     * 配置拦截文件
+     * @param registry
+     */
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(checkLoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/apptab/login.html");
-    }*/
-
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
     }
 
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-
-    }
-
-    @Override
-    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-
-    }
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-
-    }
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-
-    }
-
-    @Override
-    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> handlers) {
-
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-    }
-
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-
-    }
-
-    @Override
-    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
-
-    }
-
-    @Override
-    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
-
-    }
-
-    @Override
-    public Validator getValidator() {
-        return null;
-    }
-
-    @Override
-    public MessageCodesResolver getMessageCodesResolver() {
-        return null;
-    }
 }
