ce267d327fffaa21188b31cbae611a45cfdf5cbc
Joker-lly <15230108390@163.com>
2020-06-07 10:27:19 +0800

    数据库配置文件完善

--- src/main/java/com/lly/config/MyBatisConfig.java
+++ src/main/java/com/lly/config/MyBatisConfig.java
@@ -1,6 +1,8 @@
 package com.lly.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.lly.plugins.MyPagePlugin;
+import org.apache.ibatis.plugin.Interceptor;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,11 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import javax.sql.DataSource;
 import java.io.IOException;
 
+/**
+ * 数据库配置文件
+ * @author Joker-lly
+ * @since 2020-06-07
+ */
 @MapperScan("com.mapper")
 @Configuration
 public class MyBatisConfig {
@@ -20,19 +27,18 @@ public class MyBatisConfig {
         bean.setDataSource(dataSource);
         PathMatchingResourcePatternResolver resolver=new PathMatchingResourcePatternResolver();
         String packageXMLConfigPath = PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + "Mapper/*.xml";
-        //bean.setPlugins(new Interceptor[]{getMyPagePlugin()});
+        bean.setPlugins(new Interceptor[]{getMyPagePlugin()});
         bean.setMapperLocations(resolver.getResources(packageXMLConfigPath));
         return bean;
     }
 
-   /* public Interceptor getMyPagePlugin(){
+    @Bean
+    public Interceptor getMyPagePlugin(){
         MyPagePlugin myPagePlugin=new MyPagePlugin();
         myPagePlugin.setDatabaseType("mysql");
-
         myPagePlugin.setPageSqlId(".*ByPage$");
-
         return myPagePlugin;
-    }*/
+    }
 
     @Bean
     public DataSource dataSource(){
