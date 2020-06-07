e3542fb8fab347e2347059c312727b649e4c4e23
Joker-lly <15230108390@163.com>
2020-06-07 10:47:00 +0800

    测试类

--- src/main/java/com/lly/business/entity/TestEntity.java
+++ src/main/java/com/lly/business/entity/TestEntity.java
@@ -1,15 +1,18 @@
 package com.lly.business.entity;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @ConfigurationProperties(prefix = "info")
 public class TestEntity {
 
-//    @Value("${info.username}")
+    // 从配置文件中读取信息 并给其赋值
+    @Value("${info.username}")
     private String username;
-//    @Value("${info.password}")
+    @Value("${info.password}")
     private String password;
 
     public String getUsername() {
